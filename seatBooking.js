022d04e6148bca5e55d96ec6d1d2fa694ed2c224
“Awanee <Awanee.pandey@gmail.com>
2021-01-05 19:44:37 -0600

    localStorage

--- seatBooking.js
+++ seatBooking.js
@@ -2,9 +2,7 @@
 const container = document.querySelector('.container')
 
 const movies = document.getElementById('movie')
-console.log(movies)
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-console.log(seats);
 
 const count = document.getElementById('count');
 const total = document.getElementById('total');
@@ -12,22 +10,37 @@ const total = document.getElementById('total');
 let ticketPrice = + movies.value;
 // console.log(typeof ticketPrice);
 
+/* Save selected movie index and price */
+function setMovieData(movieIndex,moviePrice){
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice',moviePrice);
+}
+
+
+
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    // let sum = 0;
+    
+    /*to save the seats in local storage---
+    Copy selected seats into arr
+    Map through array
+    return a new array of indexes */
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    /* Local Storage */
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex))
+
     const selectedSeatCount = selectedSeats.length;
     count.innerText=selectedSeatCount;
-/*     for(i=0;i<selectedSeats.length;i++){
-        sum += ticketPrice;
-    }
-    total.innerText = sum; */
-
     total.innerText = selectedSeatCount * ticketPrice;
 }
 
 /* Movie Select */
 movies.addEventListener('change',(e)=>{
     ticketPrice = +e.target.value; 
+    setMovieData(e.target.selectedIndex,+e.target.value);
+    updateSelectedCount();
 })
 
 container.addEventListener('click', e=>{
@@ -35,6 +48,5 @@ container.addEventListener('click', e=>{
     !e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected');
         updateSelectedCount();
-
     }
 })
